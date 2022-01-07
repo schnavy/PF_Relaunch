9a936bafd6c6172eecb2d2aa314bbbc33da81d38
David <50714711+schnavy@users.noreply.github.com>
2022-01-07 18:44:19 +0100

    apicall gefixt

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-import express from "express";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import fetch from "node-fetch";
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fetch from 'node-fetch'
 
-const app = express();
-app.set("view engine", "ejs");
+const app = express()
+app.set('view engine', 'ejs')
 app.use(
-	express.urlencoded({
-		extended: true,
-	})
-);
+  express.urlencoded({
+    extended: true
+  })
+)
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'))
 
-app.get("/", async (req, res) => {
-
-	let url = "https://davidwahrenburg.de/api/work";
+app.get('/', async (req, res) => {
+	let url = 'https://backend.davidwahrenburg.de/api/work'
+	
 	fetch(url)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (d) {
-			console.log(d);
-			res.render("index.ejs", {
-				work: d,
-				title: "Arbeiten",
-			});		});
-
-
-});
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (d) {
+      console.log(d)
+      res.render('index.ejs', {
+        work: d,
+        title: 'Arbeiten'
+      })
+    })
+})
 
 app.listen(process.env.PORT || 3030, (req, res) => {
-	console.log("running on port 3030");
-});
+  console.log('running on port 3030')
+})
